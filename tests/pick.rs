a5ae0ad021f0024aba5c3ec61bde8819d0f2b90d
skanehira <sho19921005@gmail.com>
2024-08-10 12:47:59 +0900

    refactor: remove unnecessary blocks in tests

--- tests/pick.rs
+++ tests/pick.rs
@@ -3,26 +3,22 @@ mod utils;
 use type_utilities_rs::pick;
 
 test!(pick_partial_field, {
-    {
-        #[pick(NewS, [a])]
-        struct S {
-            a: i32,
-            b: &'static str,
-        }
-        _ = S { a: 10, b: "hello" };
-        _ = NewS { a: 1 };
+    #[pick(NewS, [a])]
+    struct S {
+        a: i32,
+        b: &'static str,
     }
+    _ = S { a: 10, b: "hello" };
+    _ = NewS { a: 1 };
 });
 
 test!(pick_all_files, {
-    {
-        #[pick(NewS, [a,b])]
-        struct S {
-            a: i32,
-            b: &'static str,
-        }
-        _ = NewS { a: 1, b: "foo" };
+    #[pick(NewS, [a,b])]
+    struct S {
+        a: i32,
+        b: &'static str,
     }
+    _ = NewS { a: 1, b: "foo" };
 });
 
 test!(pick_non_existent_field, {
